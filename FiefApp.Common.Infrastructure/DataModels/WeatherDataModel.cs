2db07ef3fc449b402f4c34191b2c606a613cbc71
phastari <jonas.rinder@gmail.com>
2019-08-19 09:10:36 +0200

    Bokslut alpha 4

--- FiefApp.Common.Infrastructure/DataModels/WeatherDataModel.cs
+++ FiefApp.Common.Infrastructure/DataModels/WeatherDataModel.cs
@@ -976,42 +976,47 @@ namespace FiefApp.Common.Infrastructure.DataModels
 
         private int CheckWeatherMod(int? i)
         {
-            if (i == 3)
+            if (i != null)
             {
-                return 8;
+                if (i == 3)
+                {
+                    return 8;
+                }
+                else if (i <= 5)
+                {
+                    return 4;
+                }
+                else if (i <= 7)
+                {
+                    return 2;
+                }
+                else if (i <= 9)
+                {
+                    return 1;
+                }
+                else if (i <= 11)
+                {
+                    return 0;
+                }
+                else if (i <= 13)
+                {
+                    return -1;
+                }
+                else if (i <= 15)
+                {
+                    return -2;
+                }
+                else if (i <= 17)
+                {
+                    return -4;
+                }
+                else
+                {
+                    return -Convert.ToInt32(Math.Floor((decimal) i / 3));
+                }
             }
-            else if (i <= 5)
-            {
-                return 4;
-            }
-            else if (i <= 7)
-            {
-                return 2;
-            }
-            else if (i <= 9)
-            {
-                return 1;
-            }
-            else if (i <= 11)
-            {
-                return 0;
-            }
-            else if (i <= 13)
-            {
-                return -1;
-            }
-            else if (i <= 15)
-            {
-                return -2;
-            }
-            else if (i <= 17)
-            {
-                return -4;
-            }
-            else
-            {
-                return -Convert.ToInt32(Math.Floor((decimal)i / 3));
-            }
+
+            return 0;
         }
 
         private void CalculateTotalWeatherHappiness()
